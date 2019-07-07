195c13432e3eb631f3e3f6f5c5c8db71823c3895
malembetirick <malembetirick@gmail.com>
2019-07-07 19:43:12 +0200

    MongoDB for java developer => Netflix app

---
+++ src/main/java/mflix/api/daos/UserDao.java
@@ -0,0 +1,203 @@
+package mflix.api.daos;
+
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.*;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+import mflix.api.models.Session;
+import mflix.api.models.User;
+import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.bson.conversions.Bson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.CollectionUtils;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Map;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+@Configuration
+public class UserDao extends AbstractMFlixDao {
+
+  private final MongoCollection<User> usersCollection;
+  //TODO> Ticket: User Management - do the necessary changes so that the sessions collection
+  //returns a Session object
+  private final MongoCollection<Session> sessionsCollection;
+
+  private final Logger log;
+
+  @Autowired
+  public UserDao(
+      MongoClient mongoClient, @Value("${spring.mongodb.database}") String databaseName) {
+    super(mongoClient, databaseName);
+    CodecRegistry pojoCodecRegistry =
+        fromRegistries(
+            MongoClientSettings.getDefaultCodecRegistry(),
+            fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+
+    usersCollection = db.getCollection("users", User.class).withCodecRegistry(pojoCodecRegistry);
+    log = LoggerFactory.getLogger(this.getClass());
+    //TODO> Ticket: User Management - implement the necessary changes so that the sessions
+    // collection returns a Session objects instead of Document objects.
+    sessionsCollection = db.getCollection("sessions", Session.class).withCodecRegistry(pojoCodecRegistry);
+  }
+
+  /**
+   * Inserts the `user` object in the `users` collection.
+   *
+   * @param user - User object to be added
+   * @return True if successful, throw IncorrectDaoOperation otherwise
+   */
+  public boolean addUser(User user){
+    //TODO > Ticket: Durable Writes -  you might want to use a more durable write concern here!
+    //TODO > Ticket: Handling Errors - make sure to only add new users
+    // and not users that already exist.
+    boolean result;
+    try{
+      usersCollection.insertOne(user);
+      result = true;
+    }catch (MongoWriteException m){
+      result = false;
+      throw new IncorrectDaoOperation("dup key", m);
+    }
+  return result;
+  }
+
+  /**
+   * Creates session using userId and jwt token.
+   *
+   * @param userId - user string identifier
+   * @param jwt - jwt string token
+   * @return true if successful
+   */
+  public boolean createUserSession(String userId, String jwt) {
+    //TODO> Ticket: User Management - implement the method that allows session information to be
+    // stored in it's designated collection.
+    //TODO > Ticket: Handling Errors - implement a safeguard against
+    // creating a session with the same jwt token.
+    boolean result=false, updateJWT=false;
+    try {
+      Session userSession = new Session();
+      userSession.setUserId(userId);
+      userSession.setJwt(jwt);
+      sessionsCollection.insertOne(userSession);
+      result = true;
+    } catch (MongoWriteException m){
+      updateJWT = true;
+      log.error(m.getMessage());
+    }
+    if (updateJWT){
+      try{
+        sessionsCollection.updateOne(new Document("user_id", userId), Updates.set("jwt", jwt));
+        log.info("update with success");
+        result = true;
+      } catch (MongoWriteException m){
+        log.error(m.getMessage());
+        result = false;
+      }
+
+    }
+    return result;
+  }
+
+  /**
+   * Returns the User object matching the an email string value.
+   *
+   * @param email - email string to be matched.
+   * @return User object or null.
+   */
+  public User getUser(String email) {
+    Bson filterEmail = Filters.eq("email", email);
+    User user = usersCollection.find(filterEmail).first();
+    //TODO> Ticket: User Management - implement the query that returns the first User object.
+    return user;
+  }
+
+  /**
+   * Given the userId, returns a Session object.
+   *
+   * @param userId - user string identifier.
+   * @return Session object or null.
+   */
+  public Session getUserSession(String userId) {
+    //TODO> Ticket: User Management - implement the method that returns Sessions for a given
+    // userId
+    Bson filterUserID = Filters.eq("user_id", userId);
+    Session session = sessionsCollection.find(filterUserID).first();
+    return session;
+  }
+
+  public boolean deleteUserSessions(String userId) {
+    //TODO> Ticket: User Management - implement the delete user sessions method
+    boolean result;
+    try{
+      sessionsCollection.deleteOne(new Document("user_id", userId));
+      result = true;
+    } catch (MongoException e){
+      log.error(e.getMessage());
+      result = false;
+    }
+    return result;
+  }
+
+  /**
+   * Removes the user document that match the provided email.
+   *
+   * @param email - of the user to be deleted.
+   * @return true if user successfully removed
+   */
+  public boolean deleteUser(String email) {
+    // remove user sessions
+    //TODO> Ticket: User Management - implement the delete user method
+    //TODO > Ticket: Handling Errors - make this method more robust by
+    // handling potential exceptions.
+    boolean result;
+    try{
+      User user = getUser(email);
+      usersCollection.deleteOne(new Document("email", email));
+      deleteUserSessions(email);
+      result = true;
+    } catch (MongoException e){
+      log.error(e.getMessage());
+      result = false;
+    }
+    return result;
+  }
+
+  /**
+   * Updates the preferences of an user identified by `email` parameter.
+   *
+   * @param email - user to be updated email
+   * @param userPreferences - set of preferences that should be stored and replace the existing
+   *     ones. Cannot be set to null value
+   * @return User object that just been updated.
+   */
+  public boolean updateUserPreferences(String email, Map<String, ?> userPreferences) {
+    //TODO> Ticket: User Preferences - implement the method that allows for user preferences to
+    // be updated.
+    //TODO > Ticket: Handling Errors - make this method more robust by
+    // handling potential exceptions when updating an entry.
+    boolean result;
+    if (CollectionUtils.isEmpty(userPreferences)) throw new IncorrectDaoOperation("issue");
+    else {
+      try {
+        usersCollection.updateOne(new Document("email", email), Updates.set("preferences", userPreferences));
+        result = true;
+      } catch (MongoException m){
+        log.error(m.getMessage());
+        result = false;
+      }
+    }
+    return result;
+  }
+}
